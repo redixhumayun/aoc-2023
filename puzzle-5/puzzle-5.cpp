5b56eb2c9bfa436674e0135d74c19a3241b7c114
Zaid Humayun <zaidhumayun@Zaids-Laptop.local>
2023-12-06 18:50:25 +0530

    puzzle 5 part 2 completed

--- puzzle-5/puzzle-5.cpp
+++ puzzle-5/puzzle-5.cpp
@@ -10,25 +10,33 @@
 
 using MappingType = std::map<int, std::vector<std::vector<long>>>;
 
-auto recurse(MappingType& mappings, int index, long value) -> long {
-  auto it = mappings.find(index);
-  //  base case
-  if (it == mappings.end()) {
-    return value;
-  }
-  auto mapping = it->second;
-  for (const auto& map : mapping) {
-    if (value >= map[0] && value < map[0] + map[1]) {
-      // debug_log("Map ", true, map[0], " ", map[1], " ", map[2], " ");
-      long diff = value - map[0];
-      // debug_log("Diff ", true, diff);
-      long computed_value = map[2] + diff;
-      // debug_log("Computed value ", true, computed_value);
-      return recurse(mappings, ++index, computed_value);
+long iterateMappings(const MappingType& mappings, long seed) {
+  long value = seed;
+  for (int index = 0; index < mappings.size(); ++index) {
+    auto it = mappings.find(index);
+    if (it == mappings.end()) {
+      // If no mapping is found for the current index, continue to the next
+      // index
+      continue;
     }
+
+    const auto& mapping = it->second;
+    bool matched = false;
+
+    for (const auto& map : mapping) {
+      if (value >= map[0] && value < map[0] + map[1]) {
+        long diff = value - map[0];
+        value = map[2] + diff;
+        matched = true;
+        break;  // Break out of the inner loop once a match is found
+      }
+    }
+
+    // If no match was found in this mapping, continue to the next mapping with
+    // the current value
   }
-  // debug_log("Value ", true, value);
-  return recurse(mappings, ++index, value);
+
+  return value;  // Return the final computed value
 }
 
 std::vector<std::string> split(const std::string& str) {
@@ -52,6 +60,7 @@ int main() {
 
   MappingType mappings;
   std::vector<long> seed_ranges;
+  std::vector<long> seeds;
   int currentCategory = -1;
 
   while (getline(file, line)) {
@@ -73,13 +82,6 @@ int main() {
       }
     }
 
-    //  print the seeds
-    debug_log("Seeds ", true);
-    for (const auto& seed : seed_ranges) {
-      debug_log("", false, seed, " ");
-    }
-    debug_log("", true, "");
-
     // Check if the line is a header (contains "map:")
     if (line.find("map:") != std::string::npos) {
       currentCategory++;  // Move to the next category
@@ -88,12 +90,22 @@ int main() {
 
     // Parse the mapping line
     auto tokens = split(line);
-    std::cout << "Line " << line << '\n';
     if (tokens.size() == 3) {
       long destStart = std::stoll(tokens[0]);
       long srcStart = std::stoll(tokens[1]);
       long length = std::stoll(tokens[2]);
       mappings[currentCategory].push_back({srcStart, length, destStart});
+    }
+  }
+
+  //  build the actual seed values from the seed ranges
+  for (long i = 0; i < seed_ranges.size() - 1; i += 2) {
+    long start_seed = seed_ranges[i];
+    long range = seed_ranges[i + 1];
+    debug_log("Build the seeds from ", true, start_seed, " to ",
+              start_seed + range);
+    for (long j = start_seed; j <= start_seed + range; j++) {
+      seeds.push_back(j);
     }
   }
 
@@ -108,9 +120,9 @@ int main() {
   }
 
   long min_value = std::numeric_limits<long>::max();
-  for (const auto& seed : seed_ranges) {
-    long seed_value = recurse(mappings, 0, seed);
-    debug_log("Seed ", true, seed, " -> ", seed_value);
+  for (const auto& seed : seeds) {
+    long seed_value = iterateMappings(mappings, seed);
+    // debug_log("Seed ", true, seed, " -> ", seed_value);
     if (seed_value < min_value) {
       min_value = seed_value;
     }
