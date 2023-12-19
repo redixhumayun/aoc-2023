0d96be232563a4893453c0990dba63106d49c305
Zaid Humayun <zaidhumayun@Zaids-Laptop.local>
2023-12-19 19:08:25 +0530

    puzzle 18 part 2 working

--- puzzle-18/puzzle-18.cpp
+++ puzzle-18/puzzle-18.cpp
@@ -19,98 +19,16 @@ struct Instruction {
   int distance;
 };
 
-void trace_path(std::vector<std::vector<bool>>* grid,
-                std::vector<Instruction> instructions, int x, int y) {
-  for (const auto& instruction : instructions) {
-    for (int i = 0; i < instruction.distance; i++) {
-      grid->at(y).at(x) = true;
-      switch (instruction.direction) {
-        case UP:
-          y -= 1;
-          break;
-        case DOWN:
-          y += 1;
-          break;
-        case LEFT:
-          x -= 1;
-          break;
-        case RIGHT:
-          x += 1;
-          break;
-      }
-    }
+long long calculate_area_using_shoelace_formula(
+    const std::vector<std::pair<long long, long long>>& vertices) {
+  long long area = 0;
+  int n = vertices.size();
+  for (int i = 0; i < n; i++) {
+    int j = (i + 1) % n;
+    area += vertices[i].first * vertices[j].second;
+    area -= vertices[i].second * vertices[j].first;
   }
-}
-
-int calculate_outline_area(std::vector<std::vector<bool>>* grid) {
-  int area = 0;
-  for (int i = 0; i < grid->size(); i++) {
-    for (int j = 0; j < grid->at(i).size(); j++) {
-      if (grid->at(i).at(j) == 1) {
-        area++;
-      }
-    }
-  }
-  return area;
-}
-
-int calculate_interior_area(std::vector<std::vector<bool>>* grid) {
-  int area = 0;
-  for (int i = 0; i < grid->size(); i++) {
-    for (int j = 0; j < grid->at(i).size(); j++) {
-      if (grid->at(i).at(j) == 0) {
-        area++;
-      }
-    }
-  }
-  return area;
-}
-
-void flood_fill_grid(std::vector<std::vector<bool>>* grid, int x, int y) {
-  int height = grid->size();
-  int width = grid->at(0).size();
-  std::queue<pair<int, int>> q;
-  q.push({x, y});
-  grid->at(y).at(x) = true;
-
-  std::vector<std::pair<int, int>> directions = {
-      {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-  while (!q.empty()) {
-    pair<int, int> pair = q.front();
-    q.pop();
-
-    for (const auto& [dx, dy] : directions) {
-      int new_x = pair.first + dx;
-      int new_y = pair.second + dy;
-
-      if (new_x < 0 || new_x >= width || new_y < 0 || new_y >= height) {
-        continue;
-      }
-
-      if (grid->at(new_y).at(new_x) == 1) {
-        continue;
-      }
-
-      grid->at(new_y).at(new_x) = true;
-      q.push({new_x, new_y});
-    }
-  }
-}
-
-void flood_fill_boundary(std::vector<std::vector<bool>>* grid) {
-  int height = grid->size();
-  int width = grid->at(0).size();
-
-  for (int x = 0; x < width; x++) {
-    if (!grid->at(0).at(x)) flood_fill_grid(grid, x, 0);
-    if (!grid->at(height - 1).at(x)) flood_fill_grid(grid, x, height - 1);
-  }
-
-  for (int y = 0; y < height; y++) {
-    if (!grid->at(y).at(0)) flood_fill_grid(grid, 0, y);
-    if (!grid->at(y).at(width - 1)) flood_fill_grid(grid, width - 1, y);
-  }
+  return std::abs(area / 2);
 }
 
 int main() {
@@ -126,12 +44,26 @@ int main() {
   while (getline(input_file, line)) {
     std::istringstream line_stream(line);
     char direction;
-    int distance;
+    long long distance;
     std::string color;
 
     line_stream >> direction >> distance >> color;
     Instruction instruction;
 
+    // switch (direction) {
+    //   case 'U':
+    //     instruction.direction = UP;
+    //     break;
+    //   case 'D':
+    //     instruction.direction = DOWN;
+    //     break;
+    //   case 'L':
+    //     instruction.direction = LEFT;
+    //     break;
+    //   case 'R':
+    //     instruction.direction = RIGHT;
+    //     break;
+    // }
     char new_direction = color[color.size() - 2];
     switch (new_direction) {
       case '0':
@@ -147,7 +79,7 @@ int main() {
         instruction.direction = UP;
         break;
     }
-    distance = std::stoi(color.substr(2, 6), nullptr, 16);
+    distance = std::stoll(color.substr(2, 5), nullptr, 16);
     instruction.distance = distance;
     instructions.push_back(instruction);
   }
@@ -158,8 +90,11 @@ int main() {
     debug_log("", true, instruction.direction, " ", instruction.distance);
   }
 
-  int min_x, min_y, max_x, max_y = 0;
-  int x, y = 0;
+  long long x, y = 0;  //  initial coordinates
+  std::vector<std::pair<long long, long long>> vertices;
+  vertices.push_back({x, y});
+
+  long long perimeter_count = 0;
   for (const auto& instruction : instructions) {
     switch (instruction.direction) {
       case UP:
@@ -175,48 +110,13 @@ int main() {
         x += instruction.distance;
         break;
     }
-    min_x = std::min(x, min_x);
-    min_y = std::min(y, min_y);
-    max_x = std::max(x, max_x);
-    max_y = std::max(y, max_y);
+    perimeter_count += instruction.distance;
+    vertices.push_back({x, y});
   }
 
-  int width = max_x - min_x + 1;   //  adding the additional 1 to allow the
-                                   //  flood fill to actually have effect
-  int height = max_y - min_y + 1;  //  adding the additional 1 to allow the
-                                   //  flood fill to actually have effect
-
-  std::vector<std::vector<bool>> grid(height, std::vector<bool>(width, false));
-  trace_path(&grid, instructions, -min_x, -min_y);
-
-  //  print out the grid
-  debug_log("The grid", true, "");
-  for (const auto& row : grid) {
-    for (const auto& number : row) {
-      debug_log(" ", false, number);
-    }
-    debug_log("", true, "");
-  }
-
-  //  print out the min and max values of x and y
-  debug_log("The min and max values of x and y", true, "");
-  debug_log("", true, "min_x: ", min_x, " max_x: ", max_x);
-  debug_log("", true, "min_y: ", min_y, " max_y: ", max_y);
-
-  int trace_area = calculate_outline_area(&grid);
-  debug_log("The trace area is ", true, trace_area);
-
-  flood_fill_boundary(&grid);
-  debug_log("The grid", true, "");
-  for (const auto& row : grid) {
-    for (const auto& number : row) {
-      debug_log(" ", false, number);
-    }
-    debug_log("", true, "");
-  }
-  int interior_area = calculate_interior_area(&grid);
-  debug_log("The interior area is ", true, interior_area);
-
-  int total_area = trace_area + interior_area;
-  debug_log("The total area is ", false, total_area);
+  debug_log("The perimeter count is ", true, perimeter_count);
+  long long area = calculate_area_using_shoelace_formula(vertices);
+  debug_log("The internal area is ", true, area);
+  long long ans = area + perimeter_count / 2 + 1;
+  debug_log("The area is ", true, ans);
 }
